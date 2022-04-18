ca4e66b40fe3c960435ee610db82bc3f0a650582
André Baldeweg <kontakt@andrebaldeweg.de>
2022-04-18 22:15:01 +0200

    move

--- src/views/ScheduleView.vue
+++ src/views/ScheduleView.vue
@@ -5,13 +5,53 @@
     </b-container>
 
     <b-container size="m">
-      <shift-edit />
+      <!-- debug -->
+      <pre><code>{{ schedule }}</code></pre>
+      <!-- /debug -->
+
+      <table>
+        <thead>
+          <tr>
+            <th>{{ $t('day') }}</th>
+            <th>{{ $t('shifts') }}</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="day in daysInMonth" :key="day">
+            <td>{{ getDate(day) }}</td>
+            <td>
+              <b-button
+                design="text"
+                type="button"
+                v-for="(shift, index) in shifts"
+                :key="index"
+                @click="toggleCreateModal(day, index)"
+              >
+                {{ $t('add_to') }} {{ shifts[index].name }}
+              </b-button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <shift-add
+        :staffList="staffMembers"
+        :shift="create.chosenShift"
+        v-if="create.showCreateModal"
+        @update="save"
+        @close="create.showCreateModal = false"
+      />
     </b-container>
   </article>
 </template>
 
 <script>
-import ShiftEdit from '../components/schedule/Edit.vue'
+import useShift from '@/composables/useShift'
+import useStaff from '@/composables/useStaff'
+import useSchedule from '@/composables/useSchedule'
+import ShiftAdd from './../components/schedule/Add.vue'
+import { reactive } from '@vue/composition-api'
 
 export default {
   name: 'shift-view',
@@ -19,7 +59,40 @@ export default {
     title: 'Shifts',
   },
   components: {
-    ShiftEdit,
+    ShiftAdd,
+  },
+  setup() {
+    const { shifts } = useShift()
+    const { staffMembers } = useStaff()
+    const { daysInMonth, getDate, schedule, addService } = useSchedule()
+
+    const create = reactive({
+      showCreateModal: false,
+      chosenDay: null,
+      chosenShift: null,
+    })
+
+    const toggleCreateModal = (day, shift) => {
+      create.chosenDay = day
+      create.chosenShift = shifts.value[shift]
+      create.showCreateModal = !create.showCreateModal
+    }
+
+    const save = (data) => {
+      addService(create.chosenDay, data.staff, data.starttime, data.endtime)
+      create.showCreateModal = false
+    }
+
+    return {
+      daysInMonth,
+      create,
+      getDate,
+      schedule,
+      shifts,
+      staffMembers,
+      toggleCreateModal,
+      save,
+    }
   },
 }
 </script>
