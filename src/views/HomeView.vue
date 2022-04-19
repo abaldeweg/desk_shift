d6f82c03dc295c4dd5619a96c207bca8e0000557
André Baldeweg <kontakt@andrebaldeweg.de>
2022-04-19 21:40:34 +0200

    show current month

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,16 +5,25 @@
     </b-container>
 
     <b-container size="m">
-      <router-link :to="{ name: 'schedule' }">{{ $t('schedule') }}</router-link>
+      <router-link :to="{ name: 'schedule' }"
+        >{{ $t('schedule') }} ({{ month }})</router-link
+      >
     </b-container>
   </article>
 </template>
 
 <script>
+import dayjs from 'dayjs'
+
 export default {
   name: 'home-view',
   head: {
     title: 'Home',
+  },
+  setup() {
+    const month = dayjs().format('MMMM')
+
+    return { month }
   },
 }
 </script>
